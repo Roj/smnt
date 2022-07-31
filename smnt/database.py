b0aafe6ab22c2c60c01752fc086d2fdbf1acb6fa
Joaquin Torre <joa.dev@live.com>
2022-07-31 12:25:41 -0300

    DB: use connection string in config

--- smnt/database.py
+++ smnt/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from smnt.config import DB_STRING
 
 from smnt.models import Base
 
-engine = create_engine("sqlite+pysqlite:///db.db", echo=True, future=True)
+engine = create_engine(DB_STRING, future=True)
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(engine)
